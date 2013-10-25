40809745f55c1fff1e3b4debe7c4e6206817d9e4
Oktay <dumanoktay@yandex.com>
2013-10-25 11:08:39 +0300

    ilk

---
+++ src/htmlparser/HtmlParser.java
@@ -0,0 +1,85 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package htmlparser;
+
+import Entities.Vakit;
+import Veritabani.Veritabani;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.DocumentBuilder;
+import org.w3c.dom.Document;
+import org.w3c.dom.NodeList;
+import org.w3c.dom.Node;
+import org.w3c.dom.Element;
+import java.io.File;
+import java.io.InputStream;
+import java.net.URL;
+
+
+/**
+ *
+ * @author oktay
+ */
+public class HtmlParser {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        Veritabani.baglantiAc("HtmlParserPU");
+       try {
+ 
+	//File fXmlFile = new File("/home/oktay/Downloads/16741.xml");
+        URL url = new URL("http://www.namazvakti.com/XML2013/16/16741.xml");
+        InputStream stream = url.openStream();
+
+	DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+	DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
+	Document doc = dBuilder.parse(stream);
+ 
+	//optional, but recommended
+	//read this - http://stackoverflow.com/questions/13786607/normalization-in-dom-parsing-with-java-how-does-it-work
+	doc.getDocumentElement().normalize();
+ 
+	System.out.println("Root element :" + doc.getDocumentElement().getNodeName());
+ 
+	NodeList nList = doc.getElementsByTagName("prayertimes");
+ 
+	System.out.println("----------------------------");
+ 
+	for (int temp = 0; temp < nList.getLength(); temp++) {
+ 
+		Node nNode = nList.item(temp);
+ 
+		System.out.println("\nCurrent Element :" + nNode.getNodeName());
+ 
+		if (nNode.getNodeType() == Node.ELEMENT_NODE) {
+ 
+                        Element eElement = (Element) nNode;
+                        
+                       System.out.println(""+eElement.getTextContent());
+                       
+                       String icerik = eElement.getTextContent();
+                       
+                       /*Vakit vakit= new Vakit();
+                       vakit.setIcerik(icerik);
+                       
+                       Veritabani.persist(vakit);*/
+ 
+			System.out.println("Staff id : " + eElement.getAttribute("dayofyear"));
+			/*System.out.println("First Name : " + eElement.getElementsByTagName("prayertimes").item(0).getTextContent());
+			/*System.out.println("Last Name : " + eElement.getElementsByTagName("lastname").item(0).getTextContent());
+			System.out.println("Nick Name : " + eElement.getElementsByTagName("nickname").item(0).getTextContent());
+			System.out.println("Salary : " + eElement.getElementsByTagName("salary").item(0).getTextContent());*/
+ 
+		}
+	}
+    } catch (Exception e) {
+	e.printStackTrace();
+    }
+  }
+ 
+    
+}
